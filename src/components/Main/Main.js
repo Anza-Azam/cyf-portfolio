c3c94abb8ee3e6902c289ec06590e7b92d7470f0
Anza-Azam <anzaazam.nw4@gmail.com>
2021-08-26 18:54:57 -0700

    feat: main component content and styles added

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,63 @@
-import React from "react";
-import './main.css';
+import React from 'react'
+import './main.css'
 
 export const Main = () => {
   return (
     <main>
-      <div>
-        Main component will go here
+      <div className="main-wrapper">
+        <div className="main-container">
+          <h2> About Me</h2>
+          <p>
+            Sed ut perspiciatis, unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam eaque ipsa,
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas
+            sit, aspernatur aut odit aut fugit, sed quia consequuntur magni
+            dolores eos, qui ratione voluptatem sequi nesciunt, neque porro
+            quisquam est, qui dolorem ipsum, quia dolor sit amet consectetur
+            adipisci[ng] velit, sed quia non numquam [do] eius modi tempora
+            inci[di]dunt, ut labore et dolore magnam aliquam quaerat voluptatem.
+            Ut enim ad minima veniam, quis nostrum[d] exercitationem ullam
+            corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
+            consequatur? [D]Quis autem vel eum iure reprehenderit, qui in ea
+            voluptate velit esse, quam nihil molestiae consequatur, vel illum,
+            qui dolorem eum fugiat, quo voluptas nulla pariatur? [33] At vero
+            eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis
+            praesentium voluptatum deleniti atque corrupti, quos dolores et q
+          </p>
+        </div>
+        <div className="main-container bg-primary">
+          <h2> Technical Skills</h2>
+          <ul>
+            <li>Html</li>
+            <li>Css</li>
+
+            <li>Javascript</li>
+          </ul>
+        </div>
+
+        <div className="main-container">
+          <h2> About Me</h2>
+          <p>
+            Sed ut perspiciatis, unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam eaque ipsa,
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas
+            sit, aspernatur aut odit aut fugit, sed quia consequuntur magni
+            dolores eos, qui ratione voluptatem sequi nesciunt, neque porro
+            quisquam est, qui dolorem ipsum, quia dolor sit amet consectetur
+            adipisci[ng] velit, sed quia non numquam [do] eius modi tempora
+            inci[di]dunt, ut labore et dolore magnam aliquam quaerat voluptatem.
+            Ut enim ad minima veniam, quis nostrum[d] exercitationem ullam
+            corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
+            consequatur? [D]Quis autem vel eum iure reprehenderit, qui in ea
+            voluptate velit esse, quam nihil molestiae consequatur, vel illum,
+            qui dolorem eum fugiat, quo voluptas nulla pariatur? [33] At vero
+            eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis
+            praesentium voluptatum deleniti atque corrupti, quos dolores et q
+          </p>
+        </div>
       </div>
     </main>
-  );
-};
+  )
+}
