8527defec9f7adf789ef2b43889c284b531e4284
Anza-Azam <anzaazam.nw4@gmail.com>
2021-08-26 18:04:17 -0700

    feat: navigation component content and styles added

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,12 +1,41 @@
+/** @format */
+
 import React from "react";
-import './navigation.css';
+import "./navigation.css";
 
 export const Navigation = () => {
-    return (
-        <nav>
-            <div>
-            Navigation component will go here
-            </div>
-        </nav>
-    );
+	return (
+		<nav>
+			<ul>
+				<li>
+					<a
+						href="https://github.com/Anza-Azam"
+						target="_blank"
+						rel="noreferrer"
+					>
+						GitHub
+					</a>
+				</li>
+				<li>
+					<a
+						href="https://Linkedin.com/Anza-Azam"
+						target="_blank"
+						rel="noreferrer"
+					>
+						LinkedIn
+					</a>
+				</li>
+			
+				<li>
+					<a
+						href="https://codewards.com/Anza-Azam"
+						target="_blank"
+						rel="noreferrer"
+					>
+						CodeWars
+					</a>
+				</li>
+			</ul>
+		</nav>
+	);
 };
